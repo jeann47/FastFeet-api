92639e2607d25f8c478589d35eb1e286b66bf3bf
Jeann Carlos Batemarque <jeann.carlos4@gmail.com>
2020-02-13 06:31:40 -0300

    chore: User and session controller/jwt auth set

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,69 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+
+class UserController {
+    async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string()
+                .email()
+                .required(),
+            password: Yup.string().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'validation fails' });
+        }
+
+        const exist = await User.findOne({ where: { email: req.body.email } });
+        if (exist) {
+            return res
+                .status(400)
+                .json({ error: 'There is another user using this email!' });
+        }
+        const { name, email } = await User.create(req.body);
+
+        return res.json({ name, email });
+    }
+
+    async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            oldPassword: Yup.string(),
+            password: Yup.string().when('oldPassword', (oldPassword, field) =>
+                oldPassword ? field.required() : field
+            ),
+            confirmPassword: Yup.string().when('password', (password, field) =>
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            ),
+        });
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'validation fails' });
+        }
+
+        const { email, oldPassword } = req.body;
+
+        const user = await User.findByPk(req.userId);
+
+        if (email !== user.email) {
+            const exist = await User.findOne({ where: { email } });
+            if (exist)
+                return res
+                    .status(400)
+                    .json({ error: 'There is another user using this email!' });
+        }
+        if (oldPassword && !(await User.checkPass(oldPassword)))
+            return res.status(401).json({ error: 'Passoword does not match' });
+
+        const { id, name, provider } = await User.update(req.body);
+        return res.json({ id, name, email, provider });
+    }
+
+    async delete(req, res) {
+        const deleted = await User.destroy({ where: { id: req.userId } });
+        return res.json(deleted);
+    }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 
+// import authToken from './app/middlewares/auth';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ Work: true }));
+routes.post('/users', UserController.store);
+routes.post('/login', SessionController.store);
 
 export default routes;
