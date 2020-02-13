1b7db4399f45715e043dc4018baea462e09603ca
Jeann Carlos Batemarque <jeann.carlos4@gmail.com>
2020-02-13 04:50:09 -0300

    feat: Sequelize settings & conn

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+require('dotenv/config');
+
+module.exports = {
+    dialect: 'postgres',
+    host: process.env.PG_HOST,
+    username: process.env.PG_USER,
+    password: process.env.PG_PASS,
+    database: process.env.PG_DATABASE,
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
